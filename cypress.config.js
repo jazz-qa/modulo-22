cf413c256b983757626a0c5e5195ff6546921607
Jessica Bruna de Oliveira <jazzkoliver@gmail.com>
2025-07-28 22:39:48 -0300

    versions

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
+  e2e: {
+    baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    "reporter": "mochawesome",
+    "reporterOptions": {
         "reportDir": "mochawesome-report",
         "overwrite": false,
         "reportFilename": "index.html",
         "html": true,
         "json": false
     },
-  e2e: {
-    baseUrl: 'http://lojaebac.ebaconline.art.br/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
       // require('cypress-html-reporter/GenerateReport')(on, config)
